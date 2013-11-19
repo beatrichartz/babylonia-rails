0afa53b47ecddab722899c3b46571d17403fa8c0
beatrichartz <beat.richartz@gmail.com>
2013-11-19 18:48:59 +0000

    . refactoring

--- lib/babylonia/rails/validator.rb
+++ lib/babylonia/rails/validator.rb
@@ -1,19 +1,32 @@
 class LanguagesValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if options[:present]
-      is     = record.send(:"#{attribute}_hash").delete_if{|k,v| v.empty? }.keys.sort
-      should = options[:present].sort
-      unless is == should
-        record.errors[attribute] << (options[:message] || "should also be translated in #{(should - is).map(&:upcase).to_sentence}")
-      end
+    validate_languages_present(record, attribute, option[:present].sort) if options[:present]
+    validate_length_in_range(record, attribute) if options[:length].is_a?(Range)
+  end
+  
+  private
+  
+  def validate_languages_present(record, attribute, languages)
+    is     = keys_not_matching(record, attribute){|k,v| v.empty? }
+    unless is == languages
+      record.errors[attribute] << (options[:message] || "should also be translated in #{(languages - is).map(&:upcase).to_sentence}")
     end
-    
-    if options[:length].is_a?(Range)
-      all          = record.send(:"#{attribute}_hash").keys.sort
-      in_range     = record.send(:"#{attribute}_hash").dup.delete_if{|k,v| !options[:length].include?(v.size) }.keys.sort
-      unless all.empty? || all == in_range
-        record.errors[attribute] << (options[:message] || "should be between #{options[:length].first} and #{options[:length].last} characters for #{(all - in_range).map(&:upcase).to_sentence}")
-      end
+  end
+  
+  def validate_length_in_range(record, attribute, range)
+    all          = record_attribute_hash(record, attribute).keys.sort
+    in_range     = keys_not_matching(record, attribute){|k,v| !range.include?(v.size) }
+    unless all.empty? || all == in_range
+      record.errors[attribute] << (options[:message] || "should be between #{range.first} and #{range.last} characters for #{(all - in_range).map(&:upcase).to_sentence}")
     end
+  end
+  
+  def keys_not_matching record, attribute, &block
+    not_matching = record_attribute_hash(record, attribute).dup.delete_if &block
+    not_matching.keys.sort
+  end
+  
+  def record_attribute_hash record, attribute
+    record.send(:"#{attribute}_hash")
   end
 end
